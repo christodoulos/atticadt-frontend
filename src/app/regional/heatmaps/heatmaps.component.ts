153f62bc40c23fb31f26e9863dcc37e52347eb31
Spyros Tsattalios <spyros.tsattalios@gmail.com>
2024-01-25 14:38:37 +0200

    continuing...

--- src/app/regional/heatmaps/heatmaps.component.ts
+++ src/app/regional/heatmaps/heatmaps.component.ts
@@ -103,7 +103,7 @@ export class HeatmapsComponent implements OnInit, OnDestroy {
   }
 
   mapboxHeatmap(data: MyFeatureCollection, metric: string = 'temperature') {
-    this.map?.addSource('heatmap-source', {
+    this.map?.addSource('fill-source', {
       type: 'geojson',
       data: data, // Your FeatureCollection
     });
@@ -120,46 +120,21 @@ export class HeatmapsComponent implements OnInit, OnDestroy {
     console.log(this.min, this.max);
 
     this.map?.addLayer({
-      id: 'heatmap-layer',
-      type: 'heatmap',
-      source: 'heatmap-source',
+      id: 'fill-layer',
+      type: 'fill',
+      source: 'fill-source',
+      //   maxzoom: 21,
       paint: {
-        // Increase the heatmap weight based on frequency and property magnitude
-        'heatmap-weight': [
+        'fill-color': [
           'interpolate',
           ['linear'],
-          ['get', metric],
-          0,
-          0,
-          6,
-          1,
-        ],
-        // Increase the heatmap color weight weight by zoom level
-        // heatmap-intensity is a multiplier on top of heatmap-weight
-        'heatmap-intensity': ['interpolate', ['linear'], ['zoom'], 0, 1, 9, 3],
-        // Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
-        // Begin color ramp at 0-stop with a 0-transparency color to create a blur-like effect.
-        'heatmap-color': [
-          'interpolate',
-          ['linear'],
-          ['heatmap-density'],
-          0,
-          'rgba(33,102,172,0)',
-          0.2,
-          'rgb(103,169,207)',
-          0.4,
-          'rgb(209,229,240)',
-          0.6,
-          'rgb(253,219,199)',
-          0.8,
-          'rgb(239,138,98)',
-          1,
+          ['get', 'temperature'],
+          this.min,
+          'rgba(33,102,172,1)',
+          this.max,
           'rgb(178,24,43)',
         ],
-        // Adjust the heatmap radius by zoom level
-        'heatmap-radius': ['interpolate', ['linear'], ['zoom'], 0, 2, 9, 20],
-        // Transition from heatmap to circle layer by zoom level
-        'heatmap-opacity': ['interpolate', ['linear'], ['zoom'], 7, 1, 9, 0],
+        'fill-opacity': 1,
       },
     });
   }
